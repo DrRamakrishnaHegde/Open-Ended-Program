7d3a4290dee807f0b89ff418df9440887464b7cc
DrRamakrishnaHegde <101488186+DrRamakrishnaHegde@users.noreply.github.com>
2022-03-14 17:19:00 +0530

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -1,2 +1,14 @@
 #include<stdio.h>
 // Input should be entered as as input1 input 2 format only
+int main()
+{
+  int n, sum, a[10], i;
+  print("Enter number elements")
+    scan("%d", n);
+   for(i=0; i<n;i++)
+   {
+     scanf("%d", a[i]);
+  sum = sum+a[i];
+   }
+  printf("Sum = %d", sum);
+}
